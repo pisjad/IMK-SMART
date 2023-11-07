94f36cba34f508e369df6b1b928e9127c53187ee
pisjad <hafizhassajjad17@gmail.com>
2023-11-07 21:41:12 +0700

    fajar ganteng

--- src/Views/Dashboard.jsx
+++ src/Views/Dashboard.jsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
                     href="https://www.youtube.com"
                     className="text-3xl text-white text-center bg-red-500 font-bold py-2 px-6 rounded-full hover:bg-red-700 duration-300 hover:scale-110"
                 >
-                    Kamu ganteng banget
+                    Fjaar ganteng
                 </a>
             </div>
         </>
